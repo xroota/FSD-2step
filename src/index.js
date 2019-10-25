599c8fce2a224c8bb2444fa2e3ba10aa59612da0
xroota <xrootb@gmail.com>
2019-10-25 10:56:18 +0300

    Поменял названия директорий по бэм.

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@
 import './js/index'
 
 // SCSS
-import './assets/scss/main.scss'
+import './pug/blocks/main.scss'
 
 // CSS (example)
 // import './assets/css/main.css'
 
 // Vue.js
-window.Vue = require('vue')
+//window.Vue = require('vue')
 
 // Vue components (for use in html)
-Vue.component('example-component', require('./components/Example.vue').default)
+//Vue.component('example-component', require('./components/Example.vue').default)
 
 // Vue init
-const app = new Vue({
-  el: '#app'
-})
+//const app = new Vue({
+//  el: '#app'
+//})
