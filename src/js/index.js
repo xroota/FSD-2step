ef8127bbc6dd8126a473e57d18231b9b07a7f44e
xroota <xrootb@gmail.com>
2019-11-12 12:01:35 +0300

    Add pie-chart

--- src/js/index.js
+++ src/js/index.js
@@ -4,4 +4,5 @@ import '../pug/blocks/ui-slider/ui-slider'
 import  '../pug/blocks/twbs-pagination/twbs-pagination'
 require('air-datepicker');
 import  '../pug/pages/landing-page/landing-page'
+import  '../pug/blocks/pie-chart/pie-chart'
 
