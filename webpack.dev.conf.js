23706d003f1209ece65e94d15583d5e0d26a218c
xroota <xrootb@gmail.com>
2020-02-13 11:15:08 +0300

    Fix; fixes #1

--- webpack.dev.conf.js
+++ webpack.dev.conf.js
@@ -1,9 +1,8 @@
-const webpack =  require('webpack')
-const merge = require('webpack-merge')
-const baseWebpackConfig = require('./webpack.base.conf')
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const baseWebpackConfig = require('./webpack.base.conf');
 
 const devWebpackConfig = merge(baseWebpackConfig, {
-  // DEV config
   mode: 'development',
   devtool: 'cheap-module-eval-source-map',
   devServer: {
@@ -11,26 +10,22 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     port: 8081,
     overlay: {
       warnings: true,
-      errors: true
-    }
+      errors: true,
+    },
   },
   plugins: [
     new webpack.SourceMapDevToolPlugin({
-      filename: '[file].map'
+      filename: '[file].map',
     }),
     new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery",
-      "window.jQuery": "jquery",
-      "window.$": "jquery"
-      
-  })
-  ]
-})
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      'window.$': 'jquery',
+    }),
+  ],
+});
 
-module.exports = new Promise((resolve, reject) => {
-  resolve(devWebpackConfig)
-}
-
-)
-
+module.exports = new Promise((resolve) => {
+  resolve(devWebpackConfig);
+});
